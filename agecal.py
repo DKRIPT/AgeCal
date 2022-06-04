0da43d4d37ed0e4ac8ac070e7d2e314626b5ad44
DKRIPT <106816889+DKRIPT@users.noreply.github.com>
2022-06-04 21:05:54 +0530

    Create agecal.py

---
+++ agecal.py
@@ -0,0 +1,30 @@
+#author : DKRIPT
+#title  : Age Counter
+#type   : Tool
+#level  : low
+
+print("--==--==--==--==--==--==--")
+print("Age Counter")
+print("-===-===-===-===-===-===-")
+print()
+print()
+n = input(" Your Name : ")
+b = input(" Your Birth Year : ")
+c = input(" Current Year : ")
+msg = (" Your Are : ")
+msg_two = (" Year old ")
+age = (int(c) - int(b))
+x = (msg)
+y = (age)
+z = (n)
+f = (msg_two)
+t = (".")
+#now we got user info
+#use formula to calculate age 
+#birth_year - current_year = current age
+print("-=--=--=--=--=--=--=--=--=-")
+print(x + str(y) + f + n + t)
+print("-=--=--=--=--=--=--=--=--=-")
+exit()
+
+#Current Age Calculated
